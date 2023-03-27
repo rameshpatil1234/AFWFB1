704168ec0a9be3402be7c7d6574cc20452a0068b
ramesh patil <ramesh.patil223@gmail.com>
2023-03-28 00:39:28 +0530

    test_scripts for fb now

--- test_scripts/test_invalidlogin.py
+++ test_scripts/test_invalidlogin.py
@@ -14,10 +14,9 @@ class Test_InValidLogin(BaseTest):
         except:
             un = Excel.get_cell_value("data/input.xlsx", "Invalidlogin", 2, 1)
             pw = Excel.get_cell_value("data/input.xlsx", "Invalidlogin", 2, 2)
-        loginpage =LoginPage(self.driver)
+        loginpage = LoginPage(self.driver)
         loginpage.set_username(un)
         loginpage.set_password(pw)
         loginpage.click_on_loginbutton()
         result = loginpage.verify_err_msg_is_displayed(self.wait)
         assert result
-
